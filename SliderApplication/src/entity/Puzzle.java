f414f77934e928de9593414b723f624846cd7af7
Zach Boyer <zrboyer@wpi.edu>
2018-11-01 22:51:38 -0400

    Created class and first pass at constructor for Puzzle

---
+++ SliderApplication/src/entity/Puzzle.java
@@ -0,0 +1,13 @@
+package entity;
+
+import java.util.LinkedList;
+
+public class Puzzle {
+	
+	LinkedList<Piece> pieces = new LinkedList<>();
+	
+	public Puzzle(LinkedList<Piece> pieces) {
+		
+	}
+
+}
