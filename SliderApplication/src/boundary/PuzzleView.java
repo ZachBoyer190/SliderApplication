60cddb43cb04c58d9dde5485287137c704a961d9
Zach Boyer <zrboyer@wpi.edu>
2018-11-04 19:09:00 -0500

    First pass at drawing a list of pieces in the puzzle pane

--- SliderApplication/src/boundary/PuzzleView.java
+++ SliderApplication/src/boundary/PuzzleView.java
@@ -1,19 +1,46 @@
 package boundary;
 
 import java.awt.Graphics;
+import java.awt.Color;
+
+import java.util.ArrayList;
+
 import javax.swing.JPanel;
+
+import entity.*;
 
 public class PuzzleView extends JPanel{
 	
-	public PuzzleView() {	
-		
+	Model model;
+	
+	public PuzzleView(Model m) {	
+		this.model = m;
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		g.fillRect(0, 0,  400, 500);
+		ArrayList<Piece> pieces = model.getPieces();
+		for (Piece p : pieces) {
+			
+			// TODO: Make this helper function
+			if (p.isGoal()) {
+				g.setColor(Color.red);
+			}
+			else if (p == model.getActive()) {
+				g.setColor(Color.green);
+			}
+			else {
+				g.setColor(Color.black);
+			}
+			
+			g.fillRect(p.getxCoord(), p.getyCoord(), p.getsizeX(), p.getsizeY());
+			
+			
+		}
+		
 	}
+	
 
 }
