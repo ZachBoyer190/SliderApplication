4f95efaf982372edf3dda2383daed8d9e1d7ebca
Zach Boyer <zrboyer@wpi.edu>
2018-11-04 19:08:09 -0500

    Getters for puzzle attributes

--- SliderApplication/src/entity/Piece.java
+++ SliderApplication/src/entity/Piece.java
@@ -2,6 +2,8 @@ package entity;
 
 public class Piece {
 	
+	private int xCoord;
+	int yCoord;
 	int sizeX;
 	int sizeY;
 	boolean goal;
@@ -10,12 +12,35 @@ public class Piece {
 	 * Constructor for the Piece Class
 	 */
 	
-	public Piece(int sizeX, int sizeY, boolean goal) {
+	public Piece(int xCoord, int yCoord, int sizeX, int sizeY, boolean goal) {
 		
+		this.xCoord = xCoord;
+		this.yCoord = yCoord;
 		this.sizeX = sizeX;
 		this.sizeY = sizeY;
 		this.goal = goal;
-		
 	}
+
+	public int getxCoord() {
+		return xCoord;
+	}
+	
+	public int getyCoord() {
+		return yCoord;
+	}
+	
+	public int getsizeX() {
+		return sizeX;
+	}
+	
+	public int getsizeY() {
+		return sizeY;
+	}
+	
+	public boolean isGoal() {
+		return goal;
+	}
+
+	
 
 }
