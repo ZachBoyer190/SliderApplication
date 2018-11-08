c2217207e948eefdbc9369aa9074d71573131007
Zach Boyer <zrboyer@wpi.edu>
2018-11-08 01:02:45 -0500

    Initial commit for move controller

---
+++ SliderApplication/src/controller/Move.java
@@ -0,0 +1,18 @@
+package controller;
+
+import boundary.SliderApplication;
+
+public class Move {
+	
+	SliderApplication app;
+	int direction;
+	
+	public Move(SliderApplication app, int direction) {
+		this.app = app;
+		this.direction = direction;
+		
+	}
+	
+	
+
+}
